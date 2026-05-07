1760cedb26cae0aa81504e3b19dcf29167030eca
suyashh <baradsuyash4@gmail.com>
2026-05-07 19:21:27 +0530

    Implement line detection for question attributes
    Added checks to identify specific lines in the input file.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,23 @@ int main() {
     }
     string line;
     while(getline(file, line)){
-        cout<<line<<endl;
+        //.find() and string::npos finds the line with given start
+        if(line.find("\\question") != string::npos){
+            cout<<"question Line found: \n"<<line<<endl;
+        }
+        if(line.find("\\marks") != string::npos){
+            cout<<"marks Line found: \n"<<line<<endl;
+        }
+        if(line.find("\\format") != string::npos){
+            cout<<"format Line found: \n"<<line<<endl;
+        }
+        if(line.find("\\difficulty") != string::npos){
+            cout<<"difficulty Line found: \n"<<line<<endl;
+        }
+        if(line.find("\\options") != string::npos){
+            cout<<"options Line found: \n"<<line<<endl;
+        }
+
     }
     file.close();
     return 0;
